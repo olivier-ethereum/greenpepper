4b9e2194de9461e9983ab1e7b98528d4cd85d774
wattazoum <aziz@wattazoum.fr>
2016-01-08 16:40:03 +0100

    Build failing when using JDK 6 - I was using JDK 8 for developing (I know, I know :-( ) (strator-dev/greenpepper#24)

--- greenpepper-maven-runner/src/integration-test/java/com/greenpepper/integration/tests/ShadingTest.java
+++ greenpepper-maven-runner/src/integration-test/java/com/greenpepper/integration/tests/ShadingTest.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -19,13 +20,13 @@ public class ShadingTest {
         Properties info = new Properties();
         info.load(getClass().getResourceAsStream("/info.properties"));
         String shadedJar = info.getProperty("shaded.jar");
-        String issue24POM = String.join(File.separator, info.getProperty("it.tests.folder"), "test-issue24", "pom.xml");
-        String issue24TestFile = String.join(File.separator, info.getProperty("it.tests.folder"), "test-issue24", "src", "test", "specs", "right.html");
+        String issue24POM = StringUtils.join(File.separator, info.getProperty("it.tests.folder"), "test-issue24", "pom.xml");
+        String issue24TestFile = StringUtils.join(File.separator, info.getProperty("it.tests.folder"), "test-issue24", "src", "test", "specs", "right.html");
         URL cwd = getClass().getResource(".");
         File cwdFile = FileUtils.toFile(cwd);
 
         String line = String.format("java -jar %s -v -p %s %s %s", shadedJar, issue24POM, issue24TestFile,
-                String.join(File.separator, cwdFile.getAbsolutePath(), "testCommonsCodecsConflicts.html"));
+                StringUtils.join(File.separator, cwdFile.getAbsolutePath(), "testCommonsCodecsConflicts.html"));
         System.out.println(line);
         CommandLine command = CommandLine.parse(line);
         DefaultExecutor executor = new DefaultExecutor();

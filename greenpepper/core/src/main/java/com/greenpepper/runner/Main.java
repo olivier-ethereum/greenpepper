6f713a1b59cd0b18de89fb82e1124e87a188a2eb
wattazoum <aziz@wattazoum.fr>
2016-02-03 13:47:53 +0100

    Revert "Support Exit codes"
    This reverts commit ebfbc3ad57b694a22e8aa8a10a09d897165e45fd.

--- greenpepper/core/src/main/java/com/greenpepper/runner/Main.java
+++ greenpepper/core/src/main/java/com/greenpepper/runner/Main.java
@@ -1,13 +1,16 @@
 /*
  * Copyright (c) 2007 Pyxis Technologies inc.
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA,
@@ -18,33 +21,36 @@ package com.greenpepper.runner;
 
 import com.greenpepper.util.cli.ParseException;
 
-public final class Main {
+public final class Main
+{
+    private Main() {}
 
-    private Main() {
-    }
-
-    public static void main(String[] args) {
+    public static void main( String[] args )
+    {
         CommandLineRunner runner = new CommandLineRunner();
-        LoggingMonitor loggingMonitor = new LoggingMonitor(System.out, System.err);
-        SystemExitCodesMonitor exitCodesMonitor = new SystemExitCodesMonitor();
-        CompositeSpecificationRunnerMonitor monitor = new CompositeSpecificationRunnerMonitor(loggingMonitor, exitCodesMonitor);
-        runner.setMonitor(monitor);
-        try {
-            runner.run(args);
-            System.exit(exitCodesMonitor.getExitcode());
-        } catch (ParseException e) {
-            System.err.println(e.getMessage());
-            System.err.println("Try '--help' for more information.");
+        LoggingMonitor monitor = new LoggingMonitor( System.out, System.err );
+        runner.setMonitor( monitor );
+        try
+        {
+            runner.run( args );
+        }
+        catch (ParseException e)
+        {
+            System.err.println( e.getMessage() );
+            System.err.println( "Try '--help' for more information." );
 
-            if (e.getCause() != null) {
-                System.err.println("Caused by:");
+            if (e.getCause() != null)
+            {
+                System.err.println( "Caused by:" );
                 e.getCause().printStackTrace(System.err);
             }
 
-            System.exit(1);
-        } catch (Throwable t) {
-            monitor.exceptionOccured(t);
-            System.exit(1);
-        }
+			System.exit(1);
+		}
+        catch (Throwable t)
+        {
+            monitor.exceptionOccured( t );
+			System.exit(1);
+		}
     }
 }
